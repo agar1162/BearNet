0331ca9fff79dd25766739a82127451bdb4f37d4
Alberto Garcia <agarcia05@berkeley.edu>
2025-10-26 01:10:38 -0700

    Update

--- BearNet.api/src/test/java/bear_net/api/ApiApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package bear_net.api;
-
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApiApplicationTests { }
---
+++ BearNet.api/src/test/java/bear_net/api/ApplicationTests.java
@@ -0,0 +1,6 @@
+package bear_net.api;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests { }
